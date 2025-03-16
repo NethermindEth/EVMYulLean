1e0e572a0a968f70532cdccb0deef1d41c2c2b9f
Andrei Burdusa <andrei.burdusa@zoho.com>
2025-03-16 11:11:25 +0200

    Use input file for trie_root.py

--- EvmYul/EllipticCurvesPy/trie_root.py
+++ EvmYul/EllipticCurvesPy/trie_root.py
@@ -4,14 +4,19 @@ from base_types import Bytes
 from typing import (
     Optional,
 )
-n = int(sys.argv[1])
+
+fileName : Bytes = sys.argv[1]
+file = open(fileName, "r")
+n = int(sys.argv[2])
 
 trie: Trie[Bytes, Optional[Bytes]] = Trie(
         secured=False, default=None
     )
 
 for i in range(n):
-    trie_set(trie, bytes.fromhex(sys.argv[2*i+2]), bytes.fromhex(sys.argv[2*i+3]))
+    key = file.readline()
+    value = file.readline()
+    trie_set(trie, bytes.fromhex(key.strip()), bytes.fromhex(value.strip()))
 r = root(trie)
 
 print(bytes.hex(r), end = '')
--- EvmYul/State/TrieRoot.lean
+++ EvmYul/State/TrieRoot.lean
@@ -2,12 +2,23 @@ import EvmYul.PerformIO
 import EvmYul.Wheels
 
 def blobComputeTrieRoot (ws : Array (String × String)) : String :=
-  totallySafePerformIO ∘ IO.Process.run <|
-    pythonCommandOfInput ws
-  where pythonCommandOfInput (ws : Array (String × String)) : IO.Process.SpawnArgs := {
+  -- dbg_trace s!"called blobComputeTrieRoot with an array of size {ws.size}"
+  -- dbg_trace s!"called blobComputeTrieRoot with data {ws[0]!.2.length}"
+  let inputFile := "EvmYul/EllipticCurvesPy/trieInput.txt"
+  totallySafePerformIO do
+    IO.FS.withFile inputFile .write λ h ↦
+      forM ws.data λ s ↦ do
+        h.putStrLn s.1
+        h.putStrLn s.2
+    let result ← IO.Process.run (pythonCommandOfInput inputFile ws)
+    IO.FS.removeFile inputFile
+    pure result
+ where
+  pythonCommandOfInput (inputFile : String) (ws : Array (String × String)) : IO.Process.SpawnArgs := {
     cmd := "python3",
     args :=
       #["EvmYul/EllipticCurvesPy/trie_root.py"]
+        ++ #[inputFile]
         ++ #[ws.size.repr]
-        ++ (ws.map (λ (i, w) ↦ #[i, w])).join
+        -- ++ (ws.map (λ (i, w) ↦ #[i, w])).join
   }
