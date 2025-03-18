8d0839f54bd011159e6256f2a26653aab3fbb0c9
Frantisek Silvasi <silvasi.frantisek@gmail.com>
2025-03-18 14:21:24 +0100

    Fabricio test

--- lakefile.lean
+++ lakefile.lean
@@ -43,7 +43,7 @@ def buildFFILib (pkg : Package) (repo : GitRepo) (fileName : String) : FetchM (B
   let oFile := pkg.buildDir / fileName |>.addExtension "o"
   let includeArgs := #["-I", repo.dir.toString]
   let weakArgs := includeArgs
-  buildO oFile srcJob weakArgs #[] compiler getLeanTrace
+  buildO oFile srcJob weakArgs #["-fPIC"] compiler getLeanTrace
 
 def buildSha256Obj (pkg : Package) (fileName : String) := do
   buildFFILib pkg (← (←cloneSha2.fetch).await).1 fileName
@@ -55,9 +55,15 @@ extern_lib libleanffi pkg := do
   -- In the static lib we include:
   -- the `sha-256` library itself
   let sha256O ← buildSha256Obj pkg "sha-256"
+  -- the `keccak` library itself
   let keccak256 ← buildKeccak256Obj pkg "sha3"
   -- our own `ffi.c`
   let ffiO ← ffi.o.fetch
+
+  if !(←System.FilePath.pathExists "EthereumTests") then
+    dbg_trace s!"Cloning EthereumTests into a submodule." 
+    discard <| IO.Process.run {cmd := "git submodule init"}
+
   let name := nameToStaticLib "leanffi"
   buildStaticLib (pkg.nativeLibDir / name) #[sha256O, keccak256, ffiO]
 
