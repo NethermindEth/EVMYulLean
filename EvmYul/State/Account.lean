f0cd224043ec902dbbdf1c10fa1f3ff3faf7c8ee
Daniel Britten <Coda-Coda@users.noreply.github.com>
2025-08-12 17:06:02 +1200

    Make `PersistentAccountState` depend on EVM/Yul `OperationType`
    In preparation for handling CALL* operations in the Yul semantics

--- EvmYul/State/Account.lean
+++ EvmYul/State/Account.lean
@@ -4,6 +4,8 @@ import EvmYul.SpongeHash.Keccak256
 import EvmYul.UInt256
 import EvmYul.Wheels
 
+import EvmYul.Yul.Ast
+
 namespace EvmYul
 
 /--
@@ -15,12 +17,23 @@ def π : Batteries.RBSet AccountAddress compare :=
 
 inductive ToExecute where | Code (code : ByteArray) | Precompiled (precompiled : AccountAddress)
 
-structure PersistentAccountState where
+structure PersistentAccountState (τ : OperationType) where
   nonce    : UInt256
   balance  : UInt256
   storage  : Storage
-  code     : ByteArray
-deriving BEq, Inhabited, Repr
+  code     : match τ with
+              | .EVM => ByteArray
+              | .Yul => Yul.Ast.Stmt
+
+instance {τ} : Inhabited (PersistentAccountState τ) where
+  default := {
+    nonce := default
+    balance := default
+    storage := default
+    code := match τ with
+            | .EVM => default
+            | .Yul => default
+  }
 
 /--
 The `Account` data. Section 4.1.
@@ -39,14 +52,14 @@ a Merkle Tree. KEVM implemets it as just an key/value map.
 For now, we assume no global map `GM` with which `GM[code_hash] ≡ code`.
 - `code`
 -/
-structure Account extends PersistentAccountState where
+structure Account (τ : OperationType) extends PersistentAccountState τ where
   tstorage : Storage
-deriving BEq, Inhabited, Repr
+deriving Inhabited
 
-def PersistentAccountState.codeHash (self : PersistentAccountState) : UInt256 :=
+def PersistentAccountState.codeHash (self : PersistentAccountState .EVM) : UInt256 :=
   .ofNat <| fromByteArrayBigEndian (ffi.KEC self.code)
 
-def Account.codeHash (self : Account) : UInt256 :=
+def Account.codeHash (self : (Account .EVM)) : UInt256 :=
   self.toPersistentAccountState.codeHash
 
 end EvmYul
